79d309b5fa37926c4abf9f83932640c544e790c4
whiskyPoP <1225457351@qq.com>
2024-05-10 16:19:46 +0800

    first

--- ChatGptWX/chatitf.py
+++ ChatGptWX/chatitf.py
@@ -6,15 +6,31 @@ import random
 import openai
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from openai.error import ServiceUnavailableError
-config = configparser.ConfigParser()
-config.read('../conf.ini')
+# from openai.error import ServiceUnavailableError
+# config = configparser.ConfigParser()
+# config.read('../conf.ini')
 
-openai.api_key = config['openai']['api_key']
+# openai.api_key = config['openai']['api_key']
 
-temperature = 0.5  ## 是否更有创造力 0为没有 1为更加有创造力
-
-
+# temperature = 0.5  ## 是否更有创造力 0为没有 1为更加有创造力
+import os
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+from openai import AzureOpenAI
+ 
+key = "a80e905335c74fb589144adee45b9920"
+key_bytes = key.encode()
+# BASE_URL = "https://canadaazureopenai.openai.azure.com/";
+ 
+os.environ["AZURE_OPENAI_KEY"] = key_bytes.decode('utf-8')
+client = AzureOpenAI(
+  azure_endpoint = "https://canadaazureopenai.openai.azure.com/", 
+  api_key=os.environ["AZURE_OPENAI_KEY"], 
+  api_version="2024-02-15-preview"
+)
+message_text = [
+    {"role": "system", "content": "你是一位情感引导助手，擅长倾听和总结。"},
+]
 @csrf_exempt
 def call_prompt(req) -> HttpResponse:
     try:
@@ -22,9 +38,11 @@ def call_prompt(req) -> HttpResponse:
         # print("get:{}".format(req.GET))
         # print("body:{}".format(req.body))
         # return HttpResponse()
-        response = openai.Completion.create(model="text-davinci-003", prompt=json.loads(req.body)['prompt'],
-                                            temperature=temperature, stream=False,
-                                            max_tokens=3048)
+        response = client.chat.completions.create(\
+            model="kaigpt4",\
+            messages = message_text, \
+            max_tokens = 300, \
+            temperature = 0.3 )
         print("req:{}, response:{}", req.body, response)
         return HttpResponse(response["choices"][0]["text"])
     except ServiceUnavailableError as suError:
